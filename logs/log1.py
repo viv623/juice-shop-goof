9edc7a0a94db8e0c0bbe87d4d465d0b3ab7ecd9d
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2024-07-08 09:37:13 -0400

    Update log1.py

--- logs/log1.py
+++ logs/log1.py
@@ -1,4 +1,5 @@
 sqlstr="postgresql://log_service:U34Nx67343g!h93w@cloud.google.com:5432/log"
 txtPwd="myPass1234!"
+aws="2DpTkBeYtb0voaW/yFN9ipRM8xPY9vSUiXS1d5N+!moregibberishsirguhorwg"
 
 print("logfiletester")
